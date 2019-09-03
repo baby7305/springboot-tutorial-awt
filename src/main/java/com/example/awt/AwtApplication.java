390482f2c2b47e524b3ed0f2b96bd66b160d13bf
baby7305 <songhaibei7305@163.com>
2019-09-03 14:51:14 +0800

    Initial commit

---
+++ src/main/java/com/example/awt/AwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.awt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwtApplication.class, args);
+	}
+
+}
